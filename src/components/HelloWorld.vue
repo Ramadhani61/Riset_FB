de8d2cbb90d1a4632f353065e35a2071f4ee4808
unknown <imamimam@jkt1.ebdesk.com>
2022-12-01 20:30:03 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
       <!-- <v-facebook-login app-id="687646526313011"></v-facebook-login> -->
-      <v-facebook-login v-model="model" @sdk-init="handleSdkInit" app-id="687646526313011"/>
+      <v-facebook-login v-model="model" @sdk-init="handleSdkInit" app-id="687646526313011" @login="ceklogin" @logout="cekLogout"/>
       <button v-if="scope.logout && model.connected" @click="scope.logout">
         Logout
       </button>
@@ -23,11 +23,17 @@ import VFacebookLogin from 'vue-facebook-login-component'
     }),
   methods: {
     handleSdkInit({ FB, scope }) {
-      console.log(FB,"FB");
-      console.log(scope,"SCOPE");
+      // console.log(FB,"FB");
+      // console.log(scope,"SCOPE");
       this.FB = FB
       this.scope = scope
     },
+    ceklogin(data){
+      console.log(data,"datalogin");
+    },
+    cekLogout(data){
+      console.log(data,"datallog");
+    }
   },
   }
 </script>
